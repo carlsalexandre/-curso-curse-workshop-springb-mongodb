b5fca3ca1f94f298e65b3eee04bcf06ad6ca3efc
carlsalexandre <contatocarlosalexandrecb@gmail.com>
2024-05-11 00:02:21 -0300

    Nova classe AuthorDTO

--- src/main/java/com/cursoudemy/workshopmongo/config/Instantiation.java
+++ src/main/java/com/cursoudemy/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.cursoudemy.workshopmongo.config;
 
 import com.cursoudemy.workshopmongo.domain.Post;
 import com.cursoudemy.workshopmongo.domain.User;
+import com.cursoudemy.workshopmongo.dto.AuthorDTO;
 import com.cursoudemy.workshopmongo.repository.PostRepository;
 import com.cursoudemy.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2024"), "Indo viajar", "E levar meus cachorros junto.", alex);
-        Post post2 = new Post(null, sdf.parse("24/03/2024"), "Voltando da viagem", "Os cachorros aprontaram muito.", alex);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2024"), "Indo viajar", "E levar meus cachorros junto.", new AuthorDTO(alex));
+        Post post2 = new Post(null, sdf.parse("24/03/2024"), "Voltando da viagem", "Os cachorros aprontaram muito.", new AuthorDTO(alex));
+        
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
